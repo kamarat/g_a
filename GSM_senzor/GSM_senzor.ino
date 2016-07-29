bdcb8d570d54c62907c7e572483a1e075e690264
kamarat <kamarat@centrum.cz>
2016-07-29 22:45:54 +0200

    Doplnenie funkcie merania napájacieho napätia. Úprava funkcie uspatia a prerušenia.

--- GSM_senzor/GSM_senzor.ino
+++ GSM_senzor/GSM_senzor.ino
@@ -61,32 +61,86 @@ const uint8_t LED = 13;
 void odosliSpravu( void )
 {
   detachInterrupt( digitalPinToInterrupt( PIR ));
+  // Vypis na seriovu konzolu s naslednym cakanim na ukoncenie serioveho prenosu
   Serial.println( "Posielam spravu" );
-  digitalWrite( LED, HIGH );
+  Serial.print( "Napajacie napatie (mV) = " );
+  Serial.println( merajVcc() );
+  Serial.flush();
+  
+  while( digitalRead( PIR ))
+    digitalWrite( LED, HIGH );
+  
   Serial.println( "Koncim" );
+  
   #if DEBUG == 1
   #endif
-  digitalWrite( LED, LOW );
-  attachInterrupt( digitalPinToInterrupt( PIR ), odosliSpravu, RISING );
 }
 
 void zaspiTeraz( void )
 {
-  attachInterrupt( digitalPinToInterrupt( PIR ), odosliSpravu, RISING );
+  /* Mod prerusenia definuje, kedy ma byt prerusenie spustene. Su preddefinovane 4 konstanty: 
+   *  LOW to trigger the interrupt whenever the pin is low,
+   *  CHANGE to trigger the interrupt whenever the pin changes value
+   *  RISING to trigger when the pin goes from low to high,
+   *  FALLING for when the pin goes from high to low.
+   */
+  attachInterrupt( digitalPinToInterrupt( PIR ), odosliSpravu, RISING );  // nastavenie prerusenia
   delay( 100 );
-  // Volba spiaceho rezimu
-  set_sleep_mode( SLEEP_MODE_IDLE );
+  
+  /* Volba spiaceho rezimu
+   * Existuje 5 modov pouzitelnych na standardnych 8-bitovych AVR:
+   *    SLEEP_MODE_IDLE       – least power savings
+   *    SLEEP_MODE_ADC
+   *    SLEEP_MODE_PWR_SAVE
+   *    SLEEP_MODE_STANDBY
+   *    SLEEP_MODE_PWR_DOWN   – most power savings
+   */
+  set_sleep_mode( SLEEP_MODE_PWR_DOWN );
+  //cli();                // zakazanie globalnych preruseni
+  noInterrupts();       // zakazanie globalnych preruseni
+  sleep_enable();       // nastavenie spiaceho bitu - sleep enable (SE)
+  sleep_bod_disable();  // vypnutie the Brown Out Detector (BOD) pred uspatim
+  //sei();                // povolenie preruseni
+  interrupts();         // povolenie preruseni
+  
+  digitalWrite( LED, LOW );
+  #if DEBUG == 1
+    Serial.println( "Zaspavam." );
+    Serial.flush();
+  #endif
+  
+  sleep_cpu();      // uvedenie zariadenia do rezimu spanku 
+  sleep_disable();  // po prebudeni program pokracuje v tomto bode
+  //sei();
+  interrupts();
+}
 
-  // Nastavenie spiaceho bitu - sleep enable (SE)
-  sleep_enable();
-
-  // Uvedenie zariadenia do rezimu spanku
-  sleep_mode();
-
-  // Po prebudeni program pokracuje v tomto bode
-  sleep_disable();
-
-  digitalWrite( LED, HIGH );
+uint16_t merajVcc( void )
+{
+  /* Nastavenie registrov pre meranie 1,1V proti referencnemu napatiu Vcc
+   *   REFS[1:0]     Volba referencneho napatia
+   *      01           AVcc s externym kondenzatorom na AREF pine 
+   *   MUX[3:0]      Volba vstupneho kanalu
+   *     1110          1,1V (Vbg)
+   */
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV( REFS0 ) | _BV( MUX3 ) | _BV( MUX2 ) | _BV( MUX1 ); // nastavenie pre ATmega 328p
+  #endif  
+ 
+  delay( 2 );                                 // pauza pre ustalenie Vref
+  ADCSRA |= _BV( ADSC );                      // spustenie konverzie
+  while ( bit_is_set( ADCSRA,ADSC ));         // meranie
+  uint8_t lowADC  = ADCL;                     // prvy citany register ADCL - uzamknutie registra ADCH  
+  uint8_t highADC = ADCH;                     // po precitani ADCH odomknutie oboch registrov
+  uint16_t napatie = ( highADC<<8 ) | lowADC; // vytvorenie 16-bitoveho cisla z dvoch 8-bitovych
+  napatie = 1125300L / napatie;               // Vypocet Vcc (v mV); 1125300 = 1.1*1023*1000
+  return napatie;                             // Vcc v mV
 }
 
 /*== INICIALIZACIA ==
@@ -108,12 +162,12 @@ void setup()
   pinMode( LED, OUTPUT );
   digitalWrite( LED, HIGH );
 
-  // Nastavenie prerusenia
-  attachInterrupt( digitalPinToInterrupt( PIR ), odosliSpravu, RISING );
+  
+  //attachInterrupt( digitalPinToInterrupt( PIR ), odosliSpravu, RISING );
 
   Serial.begin( 115200 ); // inicializacia serioveho vystupu
   Serial.println( "Inicializacia ukoncena." );
-  delay( 500 );
+  delay( 1000 );
 }
 
 /*== HLAVNY PROGRAM ==
@@ -123,7 +177,7 @@ void loop()
 {
   // Arduino zostane prebudene sekundu, potom zaspi.
   // LED po zaspati zhasne a zasvieti pri prebudeni.
-  delay( 1000) ;
+  //delay( 1000) ;
   zaspiTeraz();
   
   #if DEBUG == 1
